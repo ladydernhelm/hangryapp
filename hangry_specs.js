cdb037a24b1632176e4416b57d25a347e8045e6c
Anna Marie Clifton <amc@Annas-MacBook-Air.local>
2015-01-03 10:55:47 -0800

    Add HangryApp to GitHub in preperation to switch from Express to Meteor

---
+++ hangry_specs.js
@@ -0,0 +1,57 @@
+
+// get my first endpoint to render my index.html
+//make a second endpoint (/results) to recieve for lat/lon & rating and return a differnet page
+//make express.js route between the two based on 
+//html form with "method" (GET) and "action" (/results)... need to build this too
+	//for the 10 options for choice 
+
+//Might have to do some data validation to affirm that I have the geolocation
+
+// And I need to tie together the visual elements to the code (CSS)
+
+
+
+// know the geolocation of the user	
+// access api (Yelp, Foursquare)
+	//send geolocation
+
+//request 20 resturants nearest 
+	//eliminate on # of stars 
+		//"rating" : number, 1-5 in .5 increments. 
+
+	//return restaurant nearest
+		//as address + name
+
+
+// I need to deploy to Heroku
+// So I need to get that figured out
+
+
+// Need to listen to user input
+	//GetLocation (so I have to make those talk)
+	//star rating tollerance 
+
+
+
+//
+
+
+
+
+////////p2
+
+
+//Need to id a returning user
+
+
+// Which means I’ll need to set up some kind of database (Parse?)
+
+// And get the user preferences stored in the database to talk to my code base 
+
+
+
+
+//tech plan
+	//What do I want to happen?
+		// Think of edge cases
+			// What do I want to return in edge cases
---
+++ server/main.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var path = require('path');  
+var app = express();
+var router = express.Router(); 
+app.use(router); 
+
+app.get('/', function (req, res) {
+  res.render(__dirname + '/');
+});
+
+router.get(function (req, res) {
+  res.render(__dirname + './results.html');
+});
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('My cool app listening at http://%s:%s', host, port);
+
+});
+
+// var http = require('http'),
+//     fs = require('fs');
+
+
+// fs.readFile('./index.html', function (err, html) {
+//     if (err) {
+//         throw err; 
+//     }       
+//     http.createServer(function(request, response) {  
+//         response.writeHeader(200, {"Content-Type": "text/html"});  
+//         response.write(html);  
+//         response.end();  
+//     }).listen(8000);
+// });
+
+/********************************
+*        Yelp API Calls
+*********************************/
+
+
+// Request API access: http://www.yelp.com/developers/getting_started/api_access
+
+var yelpAPICall = function() { 
+
+	var yelp = require("yelp").createClient({
+	  consumer_key: "QBkaRGvffd9UrOoADC2xXQ", 
+	  consumer_secret: "Xq0YkE6k4FNkY9Etf9jLSmySSjA",
+	  token: "VvNEUY3b06gNg5NZWUcSgoMDrCmt-Ibk",
+	  token_secret: "LJKvDi_W3ykgUtO6zX9qMdi8CZE"
+	});
+	var cordX = 37.7751463;
+	var cordY = -122.4262568;
+	// See http://www.yelp.com/developers/documentation/v2/search_api
+	yelp.search({sort: 1, ll: cordX + "," + cordY}, function(error, data) {
+		console.log("ERROR",error);
+	//	console.log(data);
+		var chosenBusinesses = pickBusiness(data.businesses);
+	 	
+	});
+
+	var userRatingSelection = 4.5; //input from user... how do I access that?
+
+	var pickBusiness = function(bizes){
+		var goodBiz = [];
+		var badBiz = [];
+		for (var i = 0; i < bizes.length; i++) {
+			var currentBiz = bizes[i];
+			if (currentBiz.rating >= userRatingSelection) {
+				goodBiz.push(
+					[currentBiz.name, currentBiz.rating, currentBiz.distance]
+				);
+			}
+			else {
+				badBiz.push(
+					[currentBiz.name, currentBiz.rating]
+				);
+			}
+		}
+	 	console.log("GOOOOOOOODDDD BUZZ **************", goodBiz[0]);
+	 	console.log("BAAAAAAAAAADD BUZZ &&&&&&&&&&&&&&", badBiz);
+
+	 	return goodBiz[0]; 
+	 	 
+
+	};
+};
