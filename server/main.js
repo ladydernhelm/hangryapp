5e744ee22cc930950ba03e06ecce286b8d8eac0b
Anna Marie Clifton <anna.marie.clifton@gmail.com>
2015-01-07 22:44:53 -0800

    cutting out bits of cruft that were left over and making the whitespace uniform

--- server/main.js
+++ server/main.js
@@ -1,25 +1,41 @@
+/*******************************************
+*                                          *
+*                HANGRY                    *
+*                                          *
+********************************************/
+
+//Express init and server setup
 var express = require('express');
 var app = express();
-var router = express.Router();
 
+var server = app.listen(3000, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('My cool app listening at http://%s:%s', host, port);
+});
+
+//Routing setup for index.html
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+//Routing set up for results.html 
 app.get('/results', function(req, res) {
-
 	var latField = req.query.latField;
 	var lonField = req.query.lonField;
 	var rating = req.query.rating; 
 
+	//data validation with geoerror end point
 	if(!latField || !lonField){
 		res.status(500).sendFile(__dirname + "/geoerror.html");
 	};
 
+	//data validation with ratingerroe end point
 	if(!rating){
 		res.status(500).sendFile("./ratingerror.html");
 	};
 
+	//calling my YelpAPICall function
 	yelpAPICall(latField, lonField, rating, function(error, chosenBusinesses){
 		//Need to change this to .render0
 		res.send(chosenBusinesses);
@@ -29,27 +45,14 @@ app.get('/results', function(req, res) {
 
 });
 
-
-
-var server = app.listen(3000, function() {
-
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('My cool app listening at http://%s:%s', host, port);
-
-});
-
-
 /********************************
 *        Yelp API Calls
 *********************************/
 
-
-// // Request API access: http://www.yelp.com/developers/getting_started/api_access
-
+//request API access: http://www.yelp.com/developers/getting_started/api_access
 var yelpAPICall = function(latField, lonField, rating, callback) { 
 
+	//Oops!! Shouldn't have this on github!
 	var yelp = require("yelp").createClient({
 	  consumer_key: "QBkaRGvffd9UrOoADC2xXQ", 
 	  consumer_secret: "Xq0YkE6k4FNkY9Etf9jLSmySSjA",
@@ -57,10 +60,19 @@ var yelpAPICall = function(latField, lonField, rating, callback) {
 	  token_secret: "LJKvDi_W3ykgUtO6zX9qMdi8CZE"
 	});
 	
-	// See http://www.yelp.com/developers/documentation/v2/search_api
-	
+
+	/** 
+	* Sorting by yelp settings. Returns 20 businesses
+	* "sort: 1" will sort by nearest to furthest. 
+	* "ll:" is looking for latitude + longitude" 
+	* (see http://www.yelp.com/developers/documentation/v2/search_api for more params if needed)
+	*/
 	var query = {sort: 1, ll: latField + "," + lonField};
 	
+
+
+	//stoped commenting here on 1/7/14... will do more tomorrow
+
 	var yelpSearchCallback = function(error, data) {
 
 		if(error){
