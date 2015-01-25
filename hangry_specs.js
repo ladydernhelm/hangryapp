763995090b3ea1c19bbbbdfc2bf253f96750c148
Anna Marie Clifton <anna.marie.clifton@gmail.com>
2015-01-25 15:23:54 -0800

    stars images and styles added

--- hangry_specs.js
+++ hangry_specs.js
@@ -1,48 +1,14 @@
 // Look into 'forever' so my server won't DIE if I get an error
 
-// get my first endpoint to render my index.html 
-//html form with "method" (GET) and "action" (/results)... need to build this too
-	//for the 10 options for choice 
-//make a second endpoint (/results) to recieve for lat/lon & rating and return a differnet page
-//make express.js route between the two
+
 //Might have to do some data validation to affirm that I have the geolocation
 
-
-
-// And I need to tie together the visual elements to the code (CSS)
-//get jQuery to bind the function to the submit function 
-//think about how to return the 
-
-
-
-
-
-
-
-
-// know the geolocation of the user	
-// access api (Yelp, Foursquare)
-	//send geolocation
-
-//request 20 resturants nearest 
-	//eliminate on # of stars 
-		//"rating" : number, 1-5 in .5 increments. 
-
-	//return restaurant nearest
-		//as address + name
-
-
 // I need to deploy to Heroku
-// So I need to get that figured out
+	// So I need to get that figured out
 
-
-// Need to listen to user input
-	//GetLocation (so I have to make those talk)
-	//star rating tollerance 
-
-
-
-//
+//Need to get images hosted
+//Need to pause the app till geolocation is loaded
+//Need to make it only return food places
 
 
 
@@ -56,7 +22,6 @@
 // Which means I’ll need to set up some kind of database (Parse?)
 
 // And get the user preferences stored in the database to talk to my code base 
-
 
 
 
